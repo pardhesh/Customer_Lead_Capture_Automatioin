4b11cd9190aaff1f298c2395b6be21c1eb57fe00
pardhesh <pardheshmaddala24@gmail.com>
2025-12-30 19:26:55 +0530

    deployment changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,3 +23,7 @@ app.include_router(admin.router)
 @app.get("/")
 def health_check():
     return {"status": "running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
